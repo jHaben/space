c18a89aec6655c2be4333d1b3a00f20d99868add
jack <jrhaben@gmail.com>
2022-12-03 14:01:22 -0600

    config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/space',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
